5d3d45eb18fba90c9e9244126ebdbc7d5f6b1b80
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-08-14 09:24:32 -0300

    Implementando o leitor de arquivo

--- src/main/java/br/com/salesiana/main/Main.java
+++ src/main/java/br/com/salesiana/main/Main.java
@@ -1,8 +1,13 @@
 package br.com.salesiana.main;
 
-public class Main {
-    public static void main(String[] args) {
+import br.com.salesiana.controller.LeitorDeArquivo;
 
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        LeitorDeArquivo leitorDeArquivo = new LeitorDeArquivo();
+        leitorDeArquivo.read("Empresas - Santa Catarina.csv");
+        System.exit(0);
         System.exit(0);
     }
 }
