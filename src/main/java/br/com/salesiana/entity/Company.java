420061235dfbdf64a65243aaba6ce15cfbc9780f
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-08-28 09:44:38 -0300

    Refatorando Company

--- src/main/java/br/com/salesiana/entity/Company.java
+++ src/main/java/br/com/salesiana/entity/Company.java
@@ -20,23 +20,44 @@ public class Company {
     @OneToMany
     private List<Fiscalization> fiscalizations;
 
-    public Company() { }
+    public Company() {
+    }
 
     public Company(String cnpj, String companyName) {
         this.cnpj = cnpj;
         this.companyName = companyName;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getCnpj() {
+        return cnpj;
     }
 
     public void setCnpj(String cnpj) {
         this.cnpj = cnpj;
     }
 
+    public String getCompanyName() {
+        return companyName;
+    }
+
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
+    }
+
+    public List<Fiscalization> getFiscalizations() {
+        return fiscalizations;
+    }
+
+    public void setFiscalizations(List<Fiscalization> fiscalizations) {
+        this.fiscalizations = fiscalizations;
     }
 
     @Override
