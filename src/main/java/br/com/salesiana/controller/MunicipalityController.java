1b2dc988e26568b8151828047599516422ebbc87
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-08-28 09:41:32 -0300

    Refatorando MunicipalityController

--- src/main/java/br/com/salesiana/controller/MunicipalityController.java
+++ src/main/java/br/com/salesiana/controller/MunicipalityController.java
@@ -1,21 +1,17 @@
 package br.com.salesiana.controller;
 
-import br.com.salesiana.dao.FederatedUnitDao;
 import br.com.salesiana.dao.MunicipalityDao;
-import br.com.salesiana.entity.FederatedUnit;
 import br.com.salesiana.entity.Municipality;
 
 public class MunicipalityController {
 
-    MunicipalityDao municipalityDao = new MunicipalityDao();
+    private MunicipalityDao municipalityDao = new MunicipalityDao();
 
-    public Municipality create( String name, Long federatedUnitId) {
+    public Municipality create(String name, Long federatedUnitId) {
         Municipality existingMunicipality = municipalityDao.getByNameAndFederatedUnit(name, federatedUnitId);
-
-        if(existingMunicipality == null) {
+        if (existingMunicipality == null) {
             municipalityDao.save(existingMunicipality);
         }
-
         return existingMunicipality;
     }
 }
