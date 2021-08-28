f256ca873215e63a137588215acc4ff50d852f24
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-08-28 09:43:46 -0300

    Refatorando FiscalizationController

--- src/main/java/br/com/salesiana/controller/FiscalizationController.java
+++ src/main/java/br/com/salesiana/controller/FiscalizationController.java
@@ -3,55 +3,25 @@ package br.com.salesiana.controller;
 import br.com.salesiana.dao.*;
 import br.com.salesiana.entity.*;
 
-import java.time.LocalDate;
-
 public class FiscalizationController {
-    private FederatedUnitDao federatedUnitDao;
-    private MunicipalityDao municipalityDao;
-    private DistrictDao districtDao;
-    private CompanyDao companyDao;
+
     private FiscalizationDao fiscalizationDao;
 
     public FiscalizationController() {
-        this.federatedUnitDao = new FederatedUnitDao();
-        this.municipalityDao = new MunicipalityDao();
-        this.districtDao = new DistrictDao();
-        this.companyDao = new CompanyDao();
         this.fiscalizationDao = new FiscalizationDao();
     }
 
-    public Fiscalization create(LocalDate date,
-                                String publicPlace,
-                                String postalCode,
-                                Company company,
-                                District district,
-                                Municipality municipality,
-                                FederatedUnit federatedUnit) throws Exception {
+    public Fiscalization create(Fiscalization fiscalizationParam) {
         try {
-
-
-
-
-
-            Fiscalization fiscalization = fiscalizationDao.getFiscalizationFromDateAndPostalCodeAndPublicPlace(date, postalCode, publicPlace);
-            if(fiscalization == null) {
-                fiscalization = new Fiscalization(
-                        date,
-                        publicPlace,
-                        postalCode,
-                        company,
-                        district,
-                        municipality,
-                        federatedUnit
-                );
-                fiscalizationDao.save(fiscalization);
+            Fiscalization fiscalization = fiscalizationDao.findByFromDateAndPostalCodeAndPublicPlace(fiscalizationParam.getDate(), fiscalizationParam.getCompany());
+            if (fiscalization == null) {
+                fiscalizationDao.save(fiscalizationParam);
+                return fiscalizationParam;
             }
-
-            return new Fiscalization();
+            return fiscalization;
         } catch (Exception err) {
             System.out.println(err.getMessage());
         }
-
         return null;
     }
 }
